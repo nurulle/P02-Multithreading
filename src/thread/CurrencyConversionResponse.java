20ced33806a88ab37b990c16b36f0258fefc2f03
nurull.e <chotimahnurul23@gmail.com>
2020-03-24 01:03:14 +0700

    per2

---
+++ src/thread/CurrencyConversionResponse.java
@@ -0,0 +1,38 @@
+package thread;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+/**
+ *
+ * @author Choco calon Presiden RI
+ */
+
+public class CurrencyConversionResponse {
+    private String base;
+    private String date;
+    private Map<String, String> rates = new TreeMap<>();
+    
+    public Map<String, String> getRates() {
+        return rates;
+    }
+    public void setRates(Map<String, String> rates){
+        this.rates = rates;
+    }
+
+    public String getBase() {
+        return base;
+    }
+
+    public void setBase(String base) {
+        this.base = base;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+}
